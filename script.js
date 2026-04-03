2292e41c34899912ea6ec0af1b4b315bd96aec05
ragulrohit <ragulrohit0620@gmail.com>
2026-04-03 22:45:42 +0530

    task1

---
+++ script.js
@@ -0,0 +1,251 @@
+var a = 10;
+a = 20;
+var a = 30;
+console.log(a);
+/* ans = 30 */
+
+
+var a = 5;
+var a = 15;
+console.log(a);
+/* ans = 15*/
+
+
+var a = 1;
+a = 2;
+a = 3;
+console.log(a);
+/* ans = 3*/
+
+
+var a = 7;
+a = 14;
+var a = 21;
+a = 28;
+console.log(a); 
+/* ans = 28*/
+
+
+var a = 100;
+var a = 200;
+a = 300;
+console.log(a); 
+/* ans = 300*/
+
+
+var a = 9;
+console.log(a); 
+/* ans = 9 */
+
+
+var a = 11;
+a = 22;
+console.log(a); 
+/* ans =22 */
+
+
+var a = 50;
+var a = 60;
+var a = 70;
+console.log(a); 
+/* ans = 70 */
+
+
+var a = 2;
+a = 4;
+a = 6;
+var a = 8;
+console.log(a); 
+/* ans = 8 */
+
+
+var a = 99;
+a = 88;
+console.log(a); 
+/* ans = 88*/
+
+/*
+let b = 10;
+b = 20;
+console.log(b); 
+/* ans =20
+
+let b = 5;
+b = 15;
+b = 25;
+console.log(b); 
+/* ans = 25
+
+
+let b = 1;
+console.log(b); 
+/* ans = 1
+
+
+let b = 7;
+b = 14;
+console.log(b); 
+/* ans = 14 
+
+
+let b = 100;
+b = 200;
+b = 300;
+console.log(b); 
+/* ans = 300
+
+
+let b = 9;
+let b = 18;
+console.log(b); 
+/ * ans = redecleration error 
+
+
+let b = 50;
+b = 60;
+let b = 70;
+console.log(b); 
+/*ans = redecleration error 
+
+
+
+let b = 2;
+b = 4;
+console.log(b); 
+// ans = 4 
+
+
+let b = 99;
+b = 88;
+b = 77;
+console.log(b); 
+// ans = 77 
+
+
+let b = 11;
+b = 22;
+console.log(b); 
+// ans = 22
+*/
+
+/*
+const c = 10;
+console.log(c); 
+// ans = 10 
+
+
+const c = 5;
+c = 15;
+console.log(c); 
+// type error assigment
+
+
+const c = 1;
+const c = 2;
+console.log(c); 
+// syntax error re- declaration 
+
+
+const c = 7;
+console.log(c); 
+// ans = 7 
+
+
+const c = 100;
+console.log(c); 
+//ans = 100
+
+
+const c = 50;
+c = 60;
+console.log(c); 
+//type error 
+
+
+const c = 9;
+console.log(c); 
+// ans = 9
+
+
+const c = 20;
+const c = 30;
+console.log(c); 
+// syntax error 
+
+
+const c = 99;
+console.log(c); 
+// ans = 99
+
+
+const c = 11;
+c = 22;
+console.log(c); 
+// type error 
+
+*/
+
+
+var a = 1;
+var a = 2;
+var a = 3;
+console.log(a); 
+//ans = 3
+
+
+var a = 10;
+a = 20;
+a = 30;
+var a = 40;
+console.log(a); 
+//ans  = 40
+
+
+var a = 5;
+a = 15;
+console.log(a); 
+// ans = 15
+
+var a = 100;
+var a = 200;
+var a = 300;
+a = 400;
+console.log(a); 
+//ans = 400
+
+
+var a = 7;
+console.log(a); 
+// ans 7
+
+/*
+let b = 3;
+b = 6;
+b = 9;
+console.log(b); 
+// ans =9
+
+
+let b = 12;
+let b = 24;
+console.log(b); 
+// syntax error
+
+
+let b = 8;
+b = 16;
+console.log(b); 
+// ans = 16 
+
+
+const c = 2;
+console.log(c); 
+// ans = 2
+
+
+const c = 4;
+c = 8;
+Console.log(c); 
+// reference error and type error //
+
+*/
